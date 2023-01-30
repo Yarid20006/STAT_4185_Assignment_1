5aa59f9a3766609b705331355e38b7d1b595e3e4
Yarid20006 <123921552+Yarid20006@users.noreply.github.com>
2023-01-30 03:05:18 -0500

    Update decrypt_message_two.py

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,18 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+
+def decrypter(cipher):
+    decrypted_message = [''] * len(cipher)
+    for i in range(len(cipher)):
+        if i % 2 == 0:
+            decrypted_message[len(cipher) - i - 1] = cipher[i]
+        else:
+            decrypted_message[i] = cipher[len(cipher) - i - 1]
+    return ''.join(decrypted_message)
+
+with open('encrypted_message_two.txt', 'r') as f:
+    encrypted_message = f.read().strip()
+    decrypted_message = decrypter(encrypted_message)
+    print(decrypted_message)
+
